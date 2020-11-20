4c9e1d1ae18328aae17e354cfb367a253b303067
sarpong <abasimisarpong@rocketmail.com>
2020-11-20 08:30:55 +0000

    Streams and more streams with Effect

--- src/main/scala/Learn.scala
+++ src/main/scala/Learn.scala
@@ -3,6 +3,8 @@ import cats.effect._
 import cats.effect.implicits._
 import cats.implicits._
 import fs2.Stream
+import scodec.bits.ByteVector.Chunk
+
 import scala.concurrent.Future
 import scala.concurrent.duration.{FiniteDuration, HOURS, MILLISECONDS, NANOSECONDS}
 
@@ -10,7 +12,7 @@ object Learn extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
     IO{
       IO {
-        Stream[fs2.Pure, Int] = Stream(1,2,3)
+        println(average(Stream(1,2,3,4,5)))
       }.unsafeRunSync()
     }.as(ExitCode.Success)
   }
@@ -29,8 +31,17 @@ object Learn extends IOApp {
     val numbers = Stream.iterate(0)((number) => number |+| 1).take(n)
     Foldable[List].fold(numbers.toList).pure[IO]
   }
-//  def traverse[G[_]: Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]]
+
   def traverse(data: Stream[fs2.Pure ,Int]) = {
     Traverse[List].traverse[Option, Int, Int](data.toList)((eachElementInData) => Option(eachElementInData))
   }
+
+  def average(someData: Stream[fs2.Pure, Int]): Int = {
+    Foldable[List].foldLeft(someData.toList, 0)((a, b) => (a + b)) / someData.toList.length
+  }
+  val effectfulStream: Stream[IO, Unit] = Stream.eval[IO, Unit](IO(println("Did this work")))
+  val convertEffectFullStreamToList = effectfulStream.compile.toList
+  val combineStreams = Stream(1,2,3,4) ++ Stream(2,4,5)
+
+  //Handling Errors using stream
 }
